6bef197430fdd15e53ad88fa6eba965d706d4c74
hemakattumenu <hemakattumenu@gmailcom>
2024-07-26 12:53:41 +0530

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,11 @@
-export const cart=[];
-function addToCart(productId) {
+export const cart=[{
+   productId :'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+   quantity:2
+} , {
+    productId:'83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
+    quantity:1
+}];
+export function addToCart(productId) {
     let matchingItem;
         cart.forEach((cartItem) =>{
           if(productId === cartItem.productId) {
