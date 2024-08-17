c33c4cb0a41d9283783a05e0a774fddfefb198e9
hemakattumenu <hemakattumenu@gmailcom>
2024-08-17 17:50:59 +0530

    Convert loadProducts from XHR to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,12 +1,13 @@
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts,loadProdctsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
 Promise.all([
+  loadProdctsFetch(),
   new Promise((resolve)=> {
     loadProducts(() => {
       resolve('value1');
